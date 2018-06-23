b577a91af4b65d16f4b0b1417137c28a9ce29e5b
Rishi_Nandedkar <nandedkar.r@husky.neu.edu>
2018-06-23 13:17:10 -0400

    Connected to Firebase database and added Http functionality

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,25 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Response } from '@angular/http';
 
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
-  selector:'app-header',
-  templateUrl:'header.component.html'
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes()
+      .subscribe(
+        (response: Response) => {
+          console.log(response);
+        }
+      );
   }
 
-)
-export class HeaderComponent{
-
+  onFetchData() {
+    this.dataStorageService.getRecipes();
+  }
 }
