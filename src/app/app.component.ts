7f91995d8a477abc783b896d44943f08022d56c5
Rishi_Nandedkar <nandedkar.r@husky.neu.edu>
2018-06-23 21:45:19 -0400

    Added in signin and signout pages, also added authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   loadedFeature = 'recipe';
 
 onNavigate(feature:string){
   this.loadedFeature = feature;
-}}
+}
+
+  ngOnInit(){
+      firebase.initializeApp({
+        apiKey: "AIzaSyAOlooUJRQgrA2MZPk_LIqni8Q-mYzhEWs",
+        authDomain: "ng-recipe-book-98cef.firebaseapp.com",
+      });
+  }
+}
